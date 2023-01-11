2b69decd5cae40ac73cbd967716756218f8a29ba
betomcp <beto.porto2000@gmail.com>
2023-01-10 22:13:17 -0300

    cinema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { HistoryComponent } from './home/components/history/history.component';
+import { MenuComponent } from './home/components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
+    HistoryComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
