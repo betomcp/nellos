5fdfa73b7ee272fe40793a0ee5dbe19ba4145179
betomcp <beto.porto2000@gmail.com>
2023-01-14 15:10:14 -0300

    teste whats

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss', './home.component-2.scss']
+  styleUrls: [
+    './home.component.scss',
+    './home.component-2.scss',
+    './responsive-scss/home-mobile.component.scss',
+    './responsive-scss/home-mobile-2.component.scss'
+  ],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
