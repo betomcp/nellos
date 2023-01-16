0bd7c535900820f94cd2640e1f87c1251edf4b15
betomcp <beto.porto2000@gmail.com>
2023-01-16 00:22:22 -0300

    cardapio responsivo

--- src/app/home/components/menu/menu.component.ts
+++ src/app/home/components/menu/menu.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss', './menu.component-2.scss']
+  styleUrls: ['./menu.component.scss', './menu.component-2.scss', './responsive-scss/menu-responsive.component.scss']
 })
 export class MenuComponent implements OnInit {
 
