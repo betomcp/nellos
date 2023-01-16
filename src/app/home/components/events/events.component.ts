a4217c347374ff1353bf1c927380c923ff2dc19c
betomcp <beto.porto2000@gmail.com>
2023-01-16 00:53:56 -0300

    eventos responsivoss

--- src/app/home/components/events/events.component.ts
+++ src/app/home/components/events/events.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+  styleUrls: ['./events.component.scss', './responsive-scss/events-responsive.component.scss']
 })
 export class EventsComponent implements OnInit {
 
