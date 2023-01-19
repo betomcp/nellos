b70a44fb2597478a05db634d26338c57e1fefab0
betomcp <beto.porto2000@gmail.com>
2023-01-19 01:42:22 -0300

    menu burger final

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public open: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu(){
+    this.open = !this.open;
   }
 
 }
