ed64b5f142cbf7487fd69183bf1060da78e97055
betomcp <beto.porto2000@gmail.com>
2023-01-23 02:38:57 -0300

    events imgs passing

--- src/app/events/events/events.component.ts
+++ src/app/events/events/events.component.ts
@@ -1,5 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+const changeImage = (index: number) => {
+  const divStyle = document.getElementById('imgs-div-id');
+  switch (index) {
+    case 1:
+      divStyle?.classList.add('img-1')
+      divStyle?.classList.remove('img-3')
+      break;
+    case 2:
+      divStyle?.classList.add('img-2')
+      divStyle?.classList.remove('img-1')
+      break;
+    case 3:
+      divStyle?.classList.add('img-3')
+      divStyle?.classList.remove('img-2')
+      break;
+  }
+};
+
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
@@ -10,7 +28,14 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class EventsComponent implements OnInit {
+  private imgIndex: number = 1;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    setInterval(() => {
+      this.imgIndex == 3 ? (this.imgIndex = 0) : (this.imgIndex += 1);
+      changeImage(this.imgIndex);
+    }, 4000);
+  }
 }
