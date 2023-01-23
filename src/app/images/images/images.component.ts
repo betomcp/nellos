eba8a7f7434b51965239406e12dbf0c546334815
betomcp <beto.porto2000@gmail.com>
2023-01-23 02:56:57 -0300

    link nas imagesn do acrdapio

--- src/app/images/images/images.component.ts
+++ src/app/images/images/images.component.ts
@@ -26,52 +26,62 @@ export class ImagesComponent implements OnInit {
       title: 'Gnocchi Arrabbiata',
       imagUrl:
         'background-image: url(assets/imgs/images/cardapio-img-1.jpg);',
-    },
+      imgLink: 'assets/imgs/images/cardapio-img-1.jpg'
+      },
     {
       title: 'Filé à Parmegiana com fritas',
       imagUrl:
         'background-image: url(assets/imgs/images/cardapio-img-2.jpg);',
-    },
+      imgLink: 'assets/imgs/images/cardapio-img-2.jpg'
+      },
     {
       title: 'Risotto Gamberi',
       imagUrl:
         'background-image: url(assets/imgs/images/cardapio-img-3.jpg);',
-    },
+      imgLink: 'assets/imgs/images/cardapio-img-3.jpg'
+      },
     {
       title: 'Papardelle Bolognese',
       imagUrl:
         'background-image: url(assets/imgs/images/cardapio-img-4.jpg);',
-    },
+      imgLink: 'assets/imgs/images/cardapio-img-4.jpg'
+      },
     {
       title: 'Scalloppine Nello com purê',
       imagUrl:
         'background-image: url(assets/imgs/images/cardapio-img-5.jpg);',
-    },
+      imgLink: 'assets/imgs/images/cardapio-img-5.jpg'
+      },
     {
       title: 'Fettuccine al Sugo Finto',
       imagUrl:
         'background-image: url(assets/imgs/images/cardapio-img-6.jpg);',
-    },
+      imgLink: 'assets/imgs/images/cardapio-img-6.jpg'
+      },
     {
       title: 'Scalloppine madeira com arroz piemontese',
       imagUrl:
         'background-image: url(assets/imgs/images/cardapio-img-7.jpg);',
-    },
+      imgLink: 'assets/imgs/images/cardapio-img-7.jpg'
+      },
     {
       title: 'Spaghetti alla Carbonara',
       imagUrl:
         'background-image: url(assets/imgs/images/cardapio-img-8.jpg);',
-    },
+      imgLink: 'assets/imgs/images/cardapio-img-8.jpg'
+      },
     {
       title: 'Pizza Margherita',
       imagUrl:
         'background-image: url(assets/imgs/images/cardapio-img-9.jpg);',
-    },
+      imgLink: 'assets/imgs/images/cardapio-img-9.jpg'
+      },
     {
       title: 'Tartufo',
       imagUrl:
         'background-image: url(assets/imgs/images/cardapio-img-10.jpg);',
-    },
+      imgLink: 'assets/imgs/images/cardapio-img-10.jpg'
+      },
   ];
 
   constructor() {}
