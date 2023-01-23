9aa579dbfa9d5cc2f0eddba470ccd4229c7f1c03
betomcp <beto.porto2000@gmail.com>
2023-01-23 01:20:08 -0300

    first responsive events

--- src/app/events/events/events.component.ts
+++ src/app/events/events/events.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss', './events.component-2.scss']
+  styleUrls: [
+    './events.component.scss',
+    './events.component-2.scss',
+    './responsive-scss/events.component-2.scss',
+  ],
 })
 export class EventsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
